555848b978d2d94aaf5da5495b65666e95e11fb4
Ajay Phogat <ajayphogat@outlook.com>
2018-06-12 13:40:17 -0400

    starting dev branch | moved css files to styles folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.png';
-import './App.css';
+import logo from './logo.png';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">DABBLE.BY.AJAY</h1>
         </header>
         <p className="App-intro">
         </p>
