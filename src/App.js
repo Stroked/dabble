e0984530ca4c495c155f6613788977d0f7e7884d
Ajayyy <ajayphogat28@gmail.com>
2019-04-07 22:01:11 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 // import logo from './logo.png';
 import './App.css';
+// import Bubbly from './Component/Bubbly';
+import D3Components from './viz/charts/D3Components'
+import Charts from './viz/linked-charts/Charts'
+
 
 class App extends Component {
+  
+state={ width: window.width, height:  window.height }
+ // state = { width: 0, height: 0 };
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions=()=> this.setState({ width: window.innerWidth, height: window.innerHeight });
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+          <D3Components></D3Components>
+          <Charts></Charts>
+        {/* <Bubbly className="bubble-force-chart"> </Bubbly> */}
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        </p>
       </div>
     );
   }
